4a199d1d0a7688cc74c9d126e60f991155da92eb
Andrew Kondrich <andrew.kondrich@scale.com>
2021-02-03 11:08:51 -0800

    make packageable

--- semantic_segmentation/network/__init__.py
+++ semantic_segmentation/network/__init__.py
@@ -6,18 +6,20 @@ import importlib
 import torch
 
 from runx.logx import logx
-from config import cfg
+from ..config import cfg
 
 
 def get_net(args, criterion):
     """
     Get Network Architecture based on arguments provided
     """
-    net = get_model(network='network.' + args.arch,
-                    num_classes=cfg.DATASET.NUM_CLASSES,
-                    criterion=criterion)
+    net = get_model(
+        network="network." + args.arch,
+        num_classes=cfg.DATASET.NUM_CLASSES,
+        criterion=criterion,
+    )
     num_params = sum([param.nelement() for param in net.parameters()])
-    logx.msg('Model params = {:2.1f}M'.format(num_params / 1000000))
+    logx.msg("Model params = {:2.1f}M".format(num_params / 1000000))
 
     net = net.cuda()
     return net
@@ -27,7 +29,7 @@ def is_gscnn_arch(args):
     """
     Network is a GSCNN network
     """
-    return 'gscnn' in args.arch
+    return "gscnn" in args.arch
 
 
 def wrap_network_in_dataparallel(net, use_apex_data_parallel=False):
@@ -36,6 +38,7 @@ def wrap_network_in_dataparallel(net, use_apex_data_parallel=False):
     """
     if use_apex_data_parallel:
         import apex
+
         net = apex.parallel.DistributedDataParallel(net)
     else:
         net = torch.nn.DataParallel(net)
@@ -46,8 +49,8 @@ def get_model(network, num_classes, criterion):
     """
     Fetch Network Function Pointer
     """
-    module = network[:network.rfind('.')]
-    model = network[network.rfind('.') + 1:]
+    module = network[: network.rfind(".")]
+    model = network[network.rfind(".") + 1 :]
     mod = importlib.import_module(module)
     net_func = getattr(mod, model)
     net = net_func(num_classes=num_classes, criterion=criterion)
